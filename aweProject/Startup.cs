635c31c1cf9996fa44bb72f58c9d8c1e86b97374
Robin Kapler <robinkapler88@gmail.com>
2021-07-01 15:42:27 +0200

    Configure IdentityRole

--- aweProject/Startup.cs
+++ aweProject/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -38,16 +39,19 @@ namespace aweProject
                     options.UseSqlServer(
                         Configuration.GetConnectionString("AppDbContextConnection")));
 
-            services.AddDefaultIdentity<AppUser>()
+            services.AddIdentity<AppUser, IdentityRole>()
                 .AddEntityFrameworkStores<AppDbContext>();
+                .AddDefaultUI();
+                .AddDefaultTokenProviders();
+                
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("AppDbContextConnection")));
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env , UserManager<AppUser> um, RoleManager<IdentityRole> rm)
         {
             if (env.IsDevelopment())
             {
